c5e6430dbc339cef3408c28a0f8ed6e813ae3184
Mansur Gadjiev <gadjetmans@gmail.com>
2012-12-04 14:14:21 +0600

    Coordinates and Cross

--- Repo/MainScreen.Designer.cs
+++ Repo/MainScreen.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Name = "MainScreen";
             this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainScreen_MouseMove);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Repo/MainScreen.cs
+++ Repo/MainScreen.cs
@@ -15,5 +15,25 @@ namespace Repo
         {
             InitializeComponent();
         }
+
+        private void MainScreen_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = Convert.ToString(e.X) +";"+ Convert.ToString(e.Y);
+        }
+        public class Krest
+        {
+            int x, y;
+            Pen p = new Pen(Color.Black);
+            public Krest(int _x,int _y)
+            {
+                this.x = _x;
+                this.y = _y;
+            }
+            public void DrawWith(Graphics g)
+            {
+                g.DrawLine(p, this.x - 5, this.y, this.x + 5, this.y);
+                g.DrawLine(p, this.x, this.y-5, this.x, this.y+5);
+            }
+        }
     }
 }
