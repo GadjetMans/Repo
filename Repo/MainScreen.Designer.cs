a67c9977ab76e20b1d953bcf3b5f04c576f601be
Mansur Gadjiev <gadjetmans@gmail.com>
2012-12-11 13:32:06 +0600

    Line

--- Repo/MainScreen.Designer.cs
+++ Repo/MainScreen.Designer.cs
@@ -28,23 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(223, 189);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(55, 17);
+            this.rb1.TabIndex = 0;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Крест";
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb_change);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(223, 212);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(57, 17);
+            this.rb2.TabIndex = 1;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Линия";
+            this.rb2.UseVisualStyleBackColor = true;
+            this.rb2.CheckedChanged += new System.EventHandler(this.rb_change);
             // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
             this.Name = "MainScreen";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainScreen_Paint);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainScreen_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainScreen_MouseMove);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
     }
 }
 
--- Repo/MainScreen.cs
+++ Repo/MainScreen.cs
@@ -11,7 +11,13 @@ namespace Repo
 {
     public partial class MainScreen : Form
     {
-        List<Krest> shape = new List<Krest>();
+        public abstract class Shape
+        {
+            public abstract void DrawWith(Graphics g);
+        }
+        List<Shape> shapes = new List<Shape>();
+        bool IsShapeStart = true;
+        Point Shapestart;
         public MainScreen()
         {
             InitializeComponent();
@@ -21,7 +27,7 @@ namespace Repo
         {
             this.Text = Convert.ToString(e.X) +";"+ Convert.ToString(e.Y);
         }
-        public class Krest
+        public class Krest:Shape
         {
             int x, y;
             Pen p = new Pen(Color.Black);
@@ -30,23 +36,54 @@ namespace Repo
                 this.x = _x;
                 this.y = _y;
             }
-            public void DrawWith(Graphics g)
+            public override void DrawWith(Graphics g)
             {
                 g.DrawLine(p, this.x - 5, this.y, this.x + 5, this.y);
                 g.DrawLine(p, this.x, this.y-5, this.x, this.y+5);
             }
         }
+        public class Line : Shape
+        {
+            Point s, f;
+            Pen p = new Pen(Color.Black);
+            public Line(Point _s, Point _f)
+            {
+                this.s = _s;
+                this.f = _f;
+            }
+            public override void DrawWith(Graphics g)
+            {
+                g.DrawLine(p, s, f);
+            }
+        }
 
         private void MainScreen_MouseDown(object sender, MouseEventArgs e)
         {
-            shape.Add(new Krest(e.X, e.Y));
+            if (rb1.Checked)
+            {
+                shapes.Add(new Krest(e.X, e.Y));
+            }
+            if (rb2.Checked)
+            {
+                if (IsShapeStart)
+                {
+                    Shapestart = e.Location;
+                }
+                else shapes.Add(new Line(Shapestart,e.Location));
+                IsShapeStart = !IsShapeStart;
+            }
             this.Refresh();
         }
 
         private void MainScreen_Paint(object sender, PaintEventArgs e)
         {
-            foreach (Krest cr in this.shape)
+            foreach (Shape cr in this.shapes)
                 cr.DrawWith(e.Graphics);
+        }
+
+        private void rb_change(object sender, EventArgs e)
+        {
+            IsShapeStart = true;
         }
     }
 }
